d919779c4410658cfdb791196fd8290898b551e4
jfloresxc <jfloresxc@gmail.com>
2025-11-27 21:18:25 -0500

    feature(lazy-loading): implementar deferables views

--- src/app/modules/ui/pages/home-page/home-page.ts
+++ src/app/modules/ui/pages/home-page/home-page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { PopularProducts } from '@/modules/products/components/popular-products/popular-products';
+import { PopularProductsSkeleton } from '@/modules/products/components/popular-products-skeleton/popular-products-skeleton';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Hero, PopularProducts],
+  imports: [Hero, PopularProducts, PopularProductsSkeleton],
   template: `
     <app-hero></app-hero>
-    <app-popular-products></app-popular-products>
+    @defer (on viewport) {
+      <app-popular-products></app-popular-products>
+    } @placeholder {
+      <app-popular-products-skeleton></app-popular-products-skeleton>
+    }
   `,
   styles: ``,
 })
