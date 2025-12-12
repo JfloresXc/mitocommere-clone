988e6fee6bf1e5558dcbffb226a0fbc052fc9496
jfloresxc <jfloresxc@gmail.com>
2025-12-11 21:12:54 -0500

    feature(app): error al importar

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,6 +9,8 @@ import { CartLocalStorageActions } from './store/cart/cart.actions';
 import { Alert } from './modules/ui/components/alert/alert';
 import { ChatBot } from './modules/ui/components/chat-bot/chat-bot';
 
+import { Action } from '@ngrx/store';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Footer, Navbar, Sidebar, ShoppingCartSidebar, Alert, ChatBot],
